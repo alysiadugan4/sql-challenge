2d91014f420c3c9a3f5439d44cc192e388e830ae
Alysia Dugan <81889411+alysiadugan4@users.noreply.github.com>
2021-10-26 14:46:31 -0700

    Add schema

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,61 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/hE0oh6
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE `departments` (
+    `dept_no` varchar(50)  NOT NULL ,
+    `dept_name` varchar(50)  NOT NULL ,
+    PRIMARY KEY (
+        `dept_no`
+    )
+);
+
+CREATE TABLE `dept_emp` (
+    `emp_no` int  NOT NULL ,
+    `dept_no` varchar(50)  NOT NULL ,
+    PRIMARY KEY (
+        `emp_no`
+    )
+);
+
+CREATE TABLE `dept_manager` (
+    `dept_no` varchar(50)  NOT NULL ,
+    `emp_no` int  NOT NULL 
+);
+
+CREATE TABLE `employees` (
+    `emp_no` int  NOT NULL ,
+    `emp_title` varchar(50)  NOT NULL ,
+    `birth_date` date  NOT NULL ,
+    `first_name` varchar(50)  NOT NULL ,
+    `last_name` varchar(50)  NOT NULL ,
+    `sex` varchar(50)  NOT NULL ,
+    `hire_date` date  NOT NULL 
+);
+
+CREATE TABLE `salaries` (
+    `emp_no` int  NOT NULL ,
+    `salary` int  NOT NULL 
+);
+
+CREATE TABLE `titles` (
+    `title_id` varchar(50)  NOT NULL ,
+    `title` varchar(50)  NOT NULL 
+);
+
+ALTER TABLE `departments` ADD CONSTRAINT `fk_departments_dept_no` FOREIGN KEY(`dept_no`)
+REFERENCES `dept_manager` (`dept_no`);
+
+ALTER TABLE `dept_emp` ADD CONSTRAINT `fk_dept_emp_emp_no` FOREIGN KEY(`emp_no`)
+REFERENCES `employees` (`emp_no`);
+
+ALTER TABLE `dept_emp` ADD CONSTRAINT `fk_dept_emp_dept_no` FOREIGN KEY(`dept_no`)
+REFERENCES `departments` (`dept_no`);
+
+ALTER TABLE `employees` ADD CONSTRAINT `fk_employees_emp_no` FOREIGN KEY(`emp_no`)
+REFERENCES `salaries` (`emp_no`);
+
+ALTER TABLE `employees` ADD CONSTRAINT `fk_employees_emp_title` FOREIGN KEY(`emp_title`)
+REFERENCES `titles` (`title_id`);
+
